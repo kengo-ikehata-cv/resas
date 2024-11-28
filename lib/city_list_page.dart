1de207ab93ddc9d567c13b126544e16b32be1d04
ikehata361 <ikehata361@gmail.com>
2024-11-28 06:24:42 +0000

    市区町村データをJsonにパースした

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myapp/city_detail_page.dart';
 import 'env.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class CityListPage extends StatefulWidget {
   const CityListPage({
@@ -28,18 +29,18 @@ class _CityListPageState extends State<CityListPage> {
 
   @override
   Widget build(BuildContext context) {
-    final cities = [
-      '札幌市',
-      '仙台市',
-      'さいたま市',
-      '千葉市',
-      '横浜市',
-      '川崎市',
-      '相模原市',
-      '新潟市',
-      '静岡市',
-      '浜松市',
-    ];
+    // final cities = [
+    //   '札幌市',
+    //   '仙台市',
+    //   'さいたま市',
+    //   '千葉市',
+    //   '横浜市',
+    //   '川崎市',
+    //   '相模原市',
+    //   '新潟市',
+    //   '静岡市',
+    //   '浜松市',
+    // ];
 
     return Scaffold(
       appBar: AppBar(title: const Text('市区町村一覧')),
@@ -51,18 +52,19 @@ class _CityListPageState extends State<CityListPage> {
                 child: CircularProgressIndicator(),
               );
             }
-            print(snapshot.data);
+            final json = jsonDecode(snapshot.data!)['result'] as List;
+            final items = json.cast<Map<String, dynamic>>();
             return ListView(children: [
-              for (final city in cities)
+              for (final city in items)
                 ListTile(
-                  title: Text(city),
+                  title: Text(city['cityName']),
                   subtitle: const Text('政令指定都市'),
                   trailing: const Icon(Icons.navigate_next),
                   onTap: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
                         builder: (context) => CityDetailPage(
-                          city: city,
+                          city: city['cityName'],
                         ),
                       ),
                     );

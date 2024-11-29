a50836839ce738d0cf22483b04b9b75b987a72e0
ikehata361 <ikehata361@gmail.com>
2024-11-29 05:08:04 +0000

    地方税をクラスに書き出してデータモデルを作成した

---
+++ lib/annual_municipality_tax.dart
@@ -0,0 +1,18 @@
+import 'package:myapp/city_type.dart';
+
+class AnnualMunicipalityTax {
+  final int year;
+  final int value;
+
+  factory AnnualMunicipalityTax.fromJson(Map<String, dynamic> json) {
+    return AnnualMunicipalityTax(
+      year: json['year'] as int,
+      value: json['value'] as int,
+    );
+  }
+
+  AnnualMunicipalityTax({
+    required this.year,
+    required this.value,
+  });
+}
--- lib/city_detail_page.dart
+++ lib/city_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
+import 'package:myapp/annual_municipality_tax.dart';
 import 'env.dart';
 import 'dart:convert';
 import 'package:myapp/city.dart';
@@ -53,15 +54,17 @@ class _CityDetailPageState extends State<CityDetailPage> {
               jsonDecode(snapshot.data!)['result'] as Map<String, dynamic>;
           final data = result['data'] as List;
           final items = data.cast<Map<String, dynamic>>();
+          final taxes = items.map(AnnualMunicipalityTax.fromJson).toList();
+
           return ListView.separated(
-            itemCount: items.length,
+            itemCount: taxes.length,
             separatorBuilder: (context, index) => const Divider(),
             itemBuilder: (context, index) {
-              final item = items[index];
+              final tax = taxes[index];
               return ListTile(
-                title: Text('${item['year']}年'),
+                title: Text('${tax.year}年'),
                 trailing: Text(
-                  _formatTaxLabel(item['value'] as int),
+                  _formatTaxLabel(tax.value),
                   style: Theme.of(context).textTheme.bodyLarge,
                 ),
               );

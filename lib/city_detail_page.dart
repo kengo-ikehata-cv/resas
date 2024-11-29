12b623218e842f1780a6eb7c28961d7d204765df
ikehata361 <ikehata361@gmail.com>
2024-11-29 04:22:59 +0000

    詳細ページをセパレート

--- lib/city_detail_page.dart
+++ lib/city_detail_page.dart
@@ -52,8 +52,9 @@ class _CityDetailPageState extends State<CityDetailPage> {
               jsonDecode(snapshot.data!)['result'] as Map<String, dynamic>;
           final data = result['data'] as List;
           final items = data.cast<Map<String, dynamic>>();
-          return ListView.builder(
+          return ListView.separated(
             itemCount: items.length,
+            separatorBuilder: (context, index) => const Divider(),
             itemBuilder: (context, index) {
               final item = items[index];
               return ListTile(

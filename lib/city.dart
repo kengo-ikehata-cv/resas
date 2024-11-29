2fcbd9504a8c07857422d9efeebd0ed827616d74
ikehata361 <ikehata361@gmail.com>
2024-11-29 03:48:23 +0000

    citytypeを使って政令指定都市の表記を適正化した

--- lib/city.dart
+++ lib/city.dart
@@ -1,22 +1,24 @@
+import 'package:myapp/city_type.dart';
+
 class City {
   final int prefCode;
   final String cityCode;
   final String cityName;
-  final String bigCityFlag;
+  final CityType cityType;
 
   factory City.fromJson(Map<String, dynamic> json) {
     return City(
       prefCode: json['prefCode'] as int,
       cityCode: json['cityCode'] as String,
       cityName: json['cityName'] as String,
-      bigCityFlag: json['bigCityFlag'] as String,
+      cityType: CityType.values[int.parse(json['bigCityFlag'] as String)],
     );
   }
-  
+
   City({
     required this.prefCode,
     required this.cityCode,
     required this.cityName,
-    required this.bigCityFlag,
+    required this.cityType,
   });
 }
